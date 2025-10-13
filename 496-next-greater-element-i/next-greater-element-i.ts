511c265b948a4cf3dd04328d6fe1c79a940a0b5f
swatiM19 <swati.munda@gmail.com>
2025-10-13 17:56:34 +0530

    Time: 43 ms (5.07%) | Memory: 63.5 MB (5.07%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.ts
+++ 496-next-greater-element-i/next-greater-element-i.ts
@@ -1,14 +1,16 @@
 function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
-    let res = Array(nums1.length).fill(-1);
+    let res = Array(nums1.length)
     const map = new Map<number, number>();
     let stack:number[] = [];
-    for(let i=0; i<nums2.length; i++){
-        while(stack.length && nums2[i]>nums2[stack[stack.length-1]]){
-            let idx = stack.pop();
-            map.set(nums2[idx],nums2[i]);
+    for(let i=nums2.length-1; i>=0; i--){
+        console.log(i)
+        while(stack.length > 0 && nums2[i] > nums2[stack[stack.length-1]]){
+            stack.pop();
         }
+        map.set(nums2[i], stack.length > 0 ? nums2[stack[stack.length - 1]] : -1);
         stack.push(i);
     }
+    console.log(res)
     for(let i=0; i<nums1.length; i++){
         res[i] = map.get(nums1[i]) ?? -1;
     }
