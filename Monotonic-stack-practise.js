af3dbe187020a0e4e644e36e645c52dcbe23fc24
Swati Munda <swatimunda@Deepanshus-MacBook-Pro-2.local>
2025-11-03 11:53:07 +0530

    NSE practise

--- Monotonic-stack-practise.js
+++ Monotonic-stack-practise.js
@@ -14,4 +14,21 @@ function findNGE(nums) {
     }
     return result;
 }
-console.log(findNGE([6, 0, 8, 1, 3]));
+// console.log(findNGE([6,0,8,1,3]));
+function findNSE(nums) {
+    var st = [];
+    var result = [];
+    for (var i = 0; i < nums.length; i++) {
+        result[i] = -1;
+    }
+    for (var i = 0; i < nums.length; i++) {
+        while (st.length > 0 && nums[i] < nums[st[st.length - 1]]) {
+            var j = st.pop();
+            result[j] = nums[i];
+        }
+        st.push(i);
+    }
+    return result;
+}
+console.log(findNSE([6, 0, 8, 1, 3])); // 0,-1,1,-1,-1
+console.log(findNSE([4, 8, 5, 2, 25])); // [2, 5, 2, -1, -1]
--- Monotonic-stack-practise.ts
+++ Monotonic-stack-practise.ts
@@ -15,4 +15,22 @@ function findNGE(nums: number[]) : number[] {
     }
     return result;
 }
-console.log(findNGE([6,0,8,1,3]));
+// console.log(findNGE([6,0,8,1,3]));
+
+function findNSE(nums: number[]): number[] {
+    let st:number[] = [];
+    let result:number[] = [];
+    for(let i=0; i<nums.length; i++){
+        result[i] = -1;
+    }
+    for(let i=0; i<nums.length; i++){
+        while(st.length > 0 && nums[i] < nums[st[st.length-1]]){
+            let j = st.pop() as number;
+            result[j] = nums[i];
+        }
+        st.push(i);
+    }
+    return result;
+}
+console.log(findNSE([6,0,8,1,3])); // 0,-1,1,-1,-1
+console.log(findNSE([4, 8, 5, 2, 25])); // [2, 5, 2, -1, -1]
