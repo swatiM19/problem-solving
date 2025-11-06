ac1fbbad7b6cb39e376b8425689a6e743f9fc2b4
Swati Munda <swatimunda@Deepanshus-MacBook-Pro-2.local>
2025-11-06 19:22:49 +0530

    code push for lru

--- Lru-cache.ts
+++ Lru-cache.ts
@@ -28,11 +28,15 @@ class LRUCache <K, V> {
         this.tail.prev = this.head;
     }
     private insert(node : N<K,V>) : void {
-
+        node.next = this.head.next;
+        node.prev = this.head;
+        this.head.next!.prev = node;
+        this.head.next = node;
     }
 
-    remove(node: N<K,V>): void {
-
+    private remove(node: N<K,V>): void {
+        node.prev!.next = node.next;
+        node.next!.prev = node.prev;
     }
     put(key : K, val: V): void {
         if (this.map.has(key)) {
@@ -43,9 +47,28 @@ class LRUCache <K, V> {
         this.map.set(key, node);
 
         if (this.map.size > this.capacity){
-            
+            const lru = this.tail.prev!;
+            this.remove(lru);
+            this.map.delete(lru.key);
         }
 
-
+        
     }
-}
+    get(key: K): V | -1 {
+        if (!this.map.has(key)) return -1;
+
+        const node = this.map.get(key)!;
+        this.remove(node);
+        this.insert(node);
+        return node.val;
+    }
+} 
+
+const cache = new LRUCache<number, string>(2);
+
+cache.put(1, "A");
+cache.put(2, "B");
+console.log(cache.get(1)); // A
+cache.put(3, "C");         // Evicts key 2
+console.log(cache.get(2)); // -1
+console.log(cache.get(3)); // C
