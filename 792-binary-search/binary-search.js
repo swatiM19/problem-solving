db9b4afc02b37a526ba90f8ead8deaf1456cb961
swatiM19 <swati.munda@gmail.com>
2025-11-10 18:30:25 +0530

    Time: 0 ms (100.00%) | Memory: 58 MB (34.23%) - LeetSync

---
+++ 792-binary-search/binary-search.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    return binarySearch(nums, target);
+};
+var binarySearch = function(nums, target){
+    let n = nums.length;
+    let low = 0; 
+    let high = n-1;
+    while(low <= high){
+        let mid = Math.floor((low+high)/2);
+        if(nums[mid] == target){
+            return mid;
+        } else if(target < nums[mid]){
+            high = mid-1;
+        } else {
+            low = mid+1;
+        }
+    }
+    return -1;
+}
