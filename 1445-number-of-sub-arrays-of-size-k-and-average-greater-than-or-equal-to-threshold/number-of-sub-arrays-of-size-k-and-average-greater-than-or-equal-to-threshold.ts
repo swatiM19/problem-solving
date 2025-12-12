cc1bc6de742e107ff05efad40999dbb860da4327
swatiM19 <swati.munda@gmail.com>
2025-12-12 20:26:17 +0530

    Time: 2 ms (67.19%) | Memory: 63.7 MB (28.13%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.ts
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.ts
@@ -1,20 +1,21 @@
 function numOfSubarrays(arr: number[], k: number, threshold: number): number {
+    if (arr.length == 0) return 0;
+    if (k > arr.length) return 0;
+    if (k <= 0) return 0;
+
     let windowSum =0;
-    let windowAvg = 0;
-    //let maxAvg = 0;
+    let target = threshold * k
     let count = 0;
     for(let i=0; i<k; i++){
         windowSum += arr[i];
     }
-    windowAvg = windowSum/k;
-    if(windowAvg >= threshold){
+    if(windowSum >= target){
         count++;
     }
     // maxAvg = windowAvg;
     for(let i=k; i<arr.length; i++){
         windowSum += arr[i] -arr[i-k];
-        windowAvg = windowSum/k;
-        if(windowAvg >= threshold){
+        if(windowSum >= target){
             count++;
         }
     }
