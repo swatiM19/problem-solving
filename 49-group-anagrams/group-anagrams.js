77db1272da5c7b5f4665b809c2654e0751a1f798
swatiM19 <swati.munda@gmail.com>
2026-01-15 17:15:00 +0530

    Time: 35 ms (49.28%) | Memory: 66.7 MB (86.51%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -3,23 +3,23 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    if(strs.length === 0) return [];
-    
-    let map = new Map();
-
-    for(let i=0; i<strs.length; i++){
-        let word = strs[i];
-        let key = word.split('').sort().join('');
-
-        if(map.has(key)){
-            map.get(key).push(word);
-        } else {
-            map.set(key, [word]);
+    if (strs.length == 0) return []
+        const map = new Map();
+        for(let i=0; i<strs.length; i++){
+            const count = new Array(26).fill(0);
+            for(let j=0; j<strs[i].length; j++){
+                count[strs[i][j].charCodeAt(0)-97]++;
+            }
+            const key = count.join('#');
+            if(map.has(key)){
+                map.get(key).push(strs[i]);
+            } else {
+                map.set(key, [strs[i]]);
+            }
         }
-    }
-    let result = [];
-    for( const [key, value] of map){
-        result.push(value);
-    }
-    return result;
+        let result = [];
+        for(const[key, value] of map){
+            result.push(value);
+        }
+        return result;
 };
