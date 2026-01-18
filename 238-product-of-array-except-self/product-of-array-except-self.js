3df5bb66b081360be171ef59bd3d5d5ed9620734
swatiM19 <swati.munda@gmail.com>
2026-01-18 16:18:00 +0530

    Time: 4 ms (85.46%) | Memory: 70.6 MB (61.17%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -4,18 +4,16 @@
  */
 var productExceptSelf = function(nums) {
     let n = nums.length;
-    let leftProd = new Array(n).fill(1);
-    let rightProd = new Array(n).fill(1);
-
-    for(let i=1; i<nums.length; i++){
-        leftProd[i] = nums[i-1] * leftProd[i-1];
-    }
-    for(let i=n-2; i>=0; i--){
-        rightProd[i] = nums[i+1] * rightProd[i+1];
-    }
     let result = new Array(n).fill(1);
-    for(let i=0; i<n; i++){
-        result[i] = leftProd[i]* rightProd[i];
+    for(let i=1; i<nums.length; i++){
+        result[i] = nums[i-1] * result[i-1];
+    }
+
+    // console.log('==',result)
+    let rightProd = 1
+    for(let i=n-1; i>=0; i--){
+        result[i] = result[i] * rightProd;
+        rightProd *= nums[i];
     }
     return result;
 };
