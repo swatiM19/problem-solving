023f4c92cb7efec639e5fdfb187abfd63b578e61
swatiM19 <swati.munda@gmail.com>
2026-01-18 23:01:03 +0530

    Time: 5 ms (59.32%) | Memory: 56.8 MB (63.06%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.js
+++ 36-valid-sudoku/valid-sudoku.js
@@ -2,50 +2,20 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-
-var validBox = function(board, sr,sc,er,ec){
-    let set = new Set();
-    for(let i=sr; i<=er; i++){
-        for(let j=sc; j<=ec; j++){
-            if(board[i][j] == '.') continue
-            if(set.has(board[i][j])){
-                return false;
-            }
-            set.add(board[i][j]);
-        }
-    }
-    return true;
-}
 var isValidSudoku = function(board) {
-    // validate row
-    for(let row = 0; row<9; row++){
-        const set = new Set();
-        for(let col = 0; col<9; col++){
-            if(board[row][col] == '.') continue;
-            if(set.has(board[row][col])){
+    const set = new Set();
+    for(let i=0; i<9; i++){
+        for(let j=0; j<9; j++){
+            if(board[i][j] == '.') continue;
+            let row_ = "row"+i+board[i][j];
+            let col_ = "col"+j+board[i][j];
+            let box_ = "box"+Math.floor(i/3)+Math.floor(j/3)+board[i][j]
+            if(set.has(row_) || set.has(col_) || set.has(box_)){
                 return false;
-            }
-            set.add(board[row][col]);
-        }
-    }
-    // validate col
-    for(let col = 0; col <9; col++){
-        const set = new Set();
-        for(let row = 0; row<9; row++){
-            if(board[row][col] == '.') continue;
-            if(set.has(board[row][col])){
-                return false;
-            }
-            set.add(board[row][col]);
-        }
-    }
-    // validate 3x3
-    for(let sr = 0; sr<9; sr+=3){
-        let er = sr+2
-        for(let sc = 0;sc<9; sc+=3){
-            let ec = sc+2
-            if(!validBox(board,sr,sc,er,ec)){
-                return false;
+            } else {
+                set.add(row_);
+                set.add(col_);
+                set.add(box_);
             }
         }
     }
