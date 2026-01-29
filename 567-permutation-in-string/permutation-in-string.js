23c9a177d196775a24873d3d02c93ce98b721f2c
swatiM19 <swati.munda@gmail.com>
2026-01-29 14:15:23 +0530

    Time: 8 ms (91.84%) | Memory: 56 MB (85.73%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.js
+++ 567-permutation-in-string/permutation-in-string.js
@@ -3,11 +3,9 @@
  * @param {string} s2
  * @return {boolean}
  */
-
-var mapsCompare = function(map1, map2){
-    if(map1.size !== map2.size) return false;
-    for(let [key, val] of map1){
-        if(map2.get(key) !== val){
+var arrEqual = function(arr1, arr2){
+    for(let i=0; i<26; i++){
+        if(arr1[i] !== arr2[i]){
             return false;
         }
     }
@@ -15,24 +13,24 @@ var mapsCompare = function(map1, map2){
 }
 var checkInclusion = function(s1, s2) {
     if(s1.length > s2.length) return false;
-    let map1 = new Map();
+    const s1Freq = new Array(26).fill(0);
+    const s2Freq = new Array(26).fill(0);
 
-    for(let i=0; i<s1.length ; i++){
-        map1.set(s1[i], (map1.get(s1[i])||0)+1);
+    for(let i=0; i<s1.length; i++){
+        s1Freq[s1.charCodeAt(i)-97]++;
     }
-    let l =0;
-    let map2 = new Map()
-    for(let r =0; r<s2.length; r++){
-        map2.set(s2[r],(map2.get(s2[r])||0)+1);
-        if(r-l+1 > s1.length){
-            map2.set(s2[l], map2.get(s2[l])-1);
-            if(map2.get(s2[l]) == 0){
-                map2.delete(s2[l]);
-            }
-            l++;
+
+    let i = 0;
+    for(let j=0; j<s2.length; j++){
+        s2Freq[s2.charCodeAt(j)-97]++;
+
+        if(j-i+1 > s1.length){
+            s2Freq[s2.charCodeAt(i) - 97]--;
+            i++;
         }
-        if(r-l+1 == s1.length){
-            if(mapsCompare(map1,map2)) return true;
+
+        if(arrEqual(s1Freq,s2Freq)){
+            return true;
         }
     }
     return false;
