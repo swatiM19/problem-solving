efe5116cca3db0d2392a7c2ff46dd5c9294de126
swatiM19 <swati.munda@gmail.com>
2026-02-08 23:17:47 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (71.90%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,8 +4,7 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    if(needle.length === 0)
-        return 0;
+    if(needle.length === 0) return 0;
     let len = haystack.length - needle.length;
     for(let i=0; i<=len; i++){
         if(haystack.slice(i,needle.length+i) == needle){
