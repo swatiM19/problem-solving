72b1e97d4a7df4fb33398ac97b42edaa6965ddf9
swatiM19 <swati.munda@gmail.com>
2026-02-12 12:12:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.js
+++ Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.js
@@ -15,20 +15,18 @@ class Solution {
     }
     
     binarySearch(arr, x){
-        let n = arr.length;
-        let low = 0; 
-        let high = n-1;
-        let ans = -1;
-        
-        while(low <= high){
-            let mid = Math.floor((low+high)/2);
-            if(arr[mid] <= x){
-            ans = mid;
-            low = mid+1
-        } else {
-            high = mid-1
-        }   
+        let s = 0;
+        let e = arr.length-1;
+        if(x < arr[0]) return -1;
+        if(arr.length == 1 && arr[0] < x) return 0
+        while(s <=e ){
+            let mid = s + Math.floor((e-s)/2);
+            if(arr[mid] > x){
+                e = mid-1;
+            } else if(arr[mid] <= x){
+                s= mid+1;
+            }
         }
-        return ans;
+        return e;
     }
 }
