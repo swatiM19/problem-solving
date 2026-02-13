7564371858af657a77fb7c9df22b6d87c9365fb2
swatiM19 <swati.munda@gmail.com>
2026-02-13 22:06:15 +0530

    Time: 0 ms (100.00%) | Memory: 56.1 MB (29.50%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let i = 0;
+    let j =0;
+    while (j < t.length){
+        if(s[i] == t[j]){
+            i++;
+        }
+        j++ ;
+    }
+    if ( i == s.length){
+        return true;
+    }
+    return false;
+};
