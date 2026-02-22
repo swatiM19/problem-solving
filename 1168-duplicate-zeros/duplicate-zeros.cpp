1c47baf5118c957d4d72983b8ab84fa03d87b543
swatiM19 <swati.munda@gmail.com>
2026-02-22 20:53:20 +0530

    Time: 2 ms (35.79%) | Memory: 14 MB (66.30%) - LeetSync

---
+++ 1168-duplicate-zeros/duplicate-zeros.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        for(int i =0 ; i< arr.size(); i++){
+            if(arr[i] == 0){
+                arr.insert(arr.begin()+i+1,0);
+                arr.erase(arr.end()-1);
+                i=i+1;
+            }
+        }
+        
+    }
+};
