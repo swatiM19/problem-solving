3617fe5d1deb05baeca43e7b7eefffe8f93ec5de
swatiM19 <swati.munda@gmail.com>
2026-02-23 21:23:17 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (29.36%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    let triangle = [];
+    triangle.push([1]);
+    for(let i=1; i<numRows; i++){
+        let prevRow = triangle[i-1];
+        let newRow = [];
+        newRow.push(1);
+        for(let j=1; j< prevRow.length; j++){
+            newRow.push(prevRow[j-1]+ prevRow[j]);
+        }
+        newRow.push(1);
+        triangle.push(newRow);
+    }
+    return triangle;
+};
