f9335d99c689607c8e3c3d8db2d8a4c2efb3a218
swatiM19 <swati.munda@gmail.com>
2026-03-10 20:49:58 +0530

    Time: 8 ms (77.49%) | Memory: 58.4 MB (45.55%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.js
+++ 907-koko-eating-bananas/koko-eating-bananas.js
@@ -4,24 +4,24 @@
  * @return {number}
  */
 var minEatingSpeed = function(piles, h) {
-  let lo = 0;
-  let hi = Math.max(...piles);
-    while(lo + 1 < hi){
-        let mid = lo + Math.floor((hi-lo)/2);
-        if(canFinishInHHour(piles, mid, h)){
-            hi = mid;
+    let low = 1;
+    let high = Math.max(...piles);
+    while(low <= high){
+        let mid = low + Math.floor((high-low)/2);
+        if(canFinish(piles,h,mid)){
+            high = mid-1;
         } else {
-            lo = mid;
+            low = mid+1;
         }
     }
-    return hi;
+    return low
 };
 
-var canFinishInHHour = function(piles, mid, h){
-    let time = 0;
+var canFinish = function(piles, h, mid) {
+    let t = 0;
     for(let i=0; i<piles.length; i++){
-        time += Math.ceil(piles[i]/mid);
-        if(time> h){
+        t += Math.ceil(piles[i]/mid);
+        if(t > h){
             return false;
         }
     }
