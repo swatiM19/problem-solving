319fc2fae867f4aea59d92fcc6865f3c07e70a1a
swatiM19 <swati.munda@gmail.com>
2026-03-13 18:11:55 +0530

    Time: 62 ms (22.73%) | Memory: 101.1 MB (52.36%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.js
+++ 1023-time-based-key-value-store/time-based-key-value-store.js
@@ -6,14 +6,22 @@ class TimeMap{
         if(!this.map[key]){
             this.map[key] = [];
         }
-        this.map[key].push({value, timestamp})
+        this.map[key].push([timestamp, value])
     }
     get(key, timestamp) {
         if(!this.map[key]) return "";
         let result = "";
-        for(let entry of this.map[key]){
-            if(entry.timestamp <= timestamp){
-                result = entry.value;
+        let entries = this.map[key];
+        let low = 0;
+        let high = entries.length-1;
+
+        while(low <= high){
+            let mid = low + Math.floor((high-low)/2);
+            if(entries[mid][0] <= timestamp){
+                result = entries[mid][1];
+                low = mid+1;
+            } else {
+                high = mid -1;
             }
         }
         return result;
