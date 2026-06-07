b3371ed4442e00c97bb6af6bc7dc51612bc346e5
swatiM19 <swati.munda@gmail.com>
2026-06-07 20:17:11 +0530

    Time: 75 ms (27.32%) | Memory: 66.9 MB (14.96%) - LeetSync

--- 200-number-of-islands/number-of-islands.js
+++ 200-number-of-islands/number-of-islands.js
@@ -3,47 +3,49 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    let n = grid.length;
-    let m = grid[0].length;
-
-    const visited = grid.map(row => row.map(num => parseInt(num)));
-    let count = 0;
-    for(let i=0; i < visited.length; i++){
-        for (let j=0; j <visited[0].length; j++){
-            if(visited[i][j]==1){
-                count++;
-                bfs([{r:i, c:j}], visited);
+    let m = grid.length;
+    let n = grid[0].length;
+    let islands = 0;
+    let vis = new Array(m);
+    for(let i=0; i<m; i++){
+        vis[i] = new Array(n).fill('0');
+    }
+    for(let i=0; i<m; i++){
+        for(let j=0; j<n; j++){
+            if(grid[i][j] == '1' && vis[i][j] == '0'){
+                // dfs(grid, vis, m, n, i, j);
+                bfs(grid, [{r:i,c:j}], vis, m, n);
+                islands++;
             }
         }
     }
-    return count;
+    return islands;
 };
-var bfs = function(queue, visited) {
-    let n = visited.length;
-    let m = visited[0].length;
+var bfs = function(grid, queue, visited, m, n){
     while(queue.length > 0){
         let {r,c} = queue.shift();
-        visited[r][c] = 0;
-        console.log('r:',r, 'c:',c)
-        for (const [delrow, delcol] of [[1, 0], [-1, 0], [0, 1], [0, -1]]) {
+        visited[r][c] = 1;
+        for(const[delrow, delcol] of [[1,0],[-1,0],[0,1],[0,-1]]){
             let nrow = delrow + r;
             let ncol = delcol + c;
-            if(nrow >=0 && nrow <n && ncol >=0 && ncol < m && visited[nrow][ncol] == 1){
-                    visited[nrow][ncol] = 0;
-                    queue.push({r:nrow, c:ncol})
-                }
+            if(nrow >= 0 && ncol >= 0 && nrow <m && ncol <n && grid[nrow][ncol] == '1' && visited[nrow][ncol] == '0'){
+                visited[nrow][ncol] = 1;
+                queue.push({r:nrow, c:ncol});
+            }
         }
-        // for(let delrow = -1; delrow <=1; delrow++){
-        //     for(let delcol = -1; delcol <=1; delcol++){
-        //         if (Math.abs(delrow) == Math.abs(delcol)) continue;
-        //         let nrow = delrow + r;
-        //         let ncol = delcol + c;
-        //         console.log('nrow:',nrow, 'ncol:',ncol)
-        //         if(nrow >=0 && nrow <n && ncol >=0 && ncol < m && visited[nrow][ncol] == 1){
-        //             visited[nrow][ncol] = 0;
-        //             queue.push({r:nrow, c:ncol})
-        //         }
-        //     }
-        // }
     }
+}
+var dfs = function(grid, vis, m, n, i, j){
+    if(i<0 || j<0 || i >=m || j >=n || grid[i][j] !== '1'){
+        return
+    }
+    if(vis[i][j] == '1'){
+        return
+    }
+    vis[i][j] = '1';
+
+    dfs(grid, vis, m, n, i+1, j);
+    dfs(grid, vis, m, n, i-1, j);
+    dfs(grid, vis, m, n, i, j+1);
+    dfs(grid, vis, m, n, i, j-1);
 }
