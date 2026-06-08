7dd41aba6484b6a5ac3027d305d7f905ddf0f7f3
swatiM19 <swati.munda@gmail.com>
2026-06-08 19:42:14 +0530

    Time: 15 ms (14.86%) | Memory: 61.6 MB (16.05%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.js
+++ 695-max-area-of-island/max-area-of-island.js
@@ -2,33 +2,39 @@
  * @param {number[][]} grid
  * @return {number}
  */
-
-var dfs = function(vis, grid, m, n, i, j) {
-    if(i<0 || j<0 ||i >=m || j >=n || grid[i][j] !== 1 || vis[i][j] == 1){
-        return 0
-    }
+var bfs = function(grid, vis, m, n, i, j){
+    let queue = [{r:i,c:j}];
     vis[i][j] = 1;
-    return 1 +
-    dfs(vis, grid, m, n, i+1, j)
-    +dfs(vis, grid, m, n, i-1, j)
-    +dfs(vis, grid, m, n, i, j+1)
-    +dfs(vis, grid, m, n, i, j-1)
+    let area = 0;
+    while(queue.length > 0){
+        const {r,c} = queue.shift();
+        area++;
+        for(const[dr,dc] of [[1,0],[-1,0],[0,1],[0,-1]]){
+            const nr = r + dr;
+            const nc = c + dc;
+            if(nr >=0 && nr <m && nc >= 0 && nc <n && grid[nr][nc] == 1 && vis[nr][nc] == 0){
+                vis[nr][nc] = 1;
+                queue.push({r:nr, c:nc});
+            }
+        } 
+    }
+    return area;
 }
 var maxAreaOfIsland = function(grid) {
     let maxArea = 0;
-    let m = grid.length;
+    let m = grid.length; 
     let n = grid[0].length;
     let vis = new Array(m);
     for(let i=0; i<m; i++){
-            vis[i] = new Array(n).fill(0);
-    };
+        vis[i] = new Array(n).fill(0);
+    }
     for(let i=0; i<m; i++){
         for(let j=0; j<n; j++){
             if(grid[i][j] == 1 && vis[i][j] == 0){
-                let area = dfs(vis, grid, m, n, i,j);
+                let area = bfs(grid, vis, m, n, i, j);
                 maxArea = Math.max(area, maxArea);
             }
         }
     }
-    return maxArea; 
+    return maxArea;
 };
