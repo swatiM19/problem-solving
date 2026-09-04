5cbcbdb74baf6e90115ec796e5405bcf9e4cf685
swatiM19 <swati.munda@gmail.com>
2026-09-04 20:37:54 +0530

    Time: 1 ms (46.67%) | Memory: 53.8 MB (59.87%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -1,8 +1,8 @@
-/**
- * @param {string} haystack
- * @param {string} needle
- * @return {number}
- */
+// /**
+//  * @param {string} haystack
+//  * @param {string} needle
+//  * @return {number}
+//  */
 var strStr = function(haystack, needle) {
     if(needle.length === 0) return 0;
     let len = haystack.length - needle.length;
